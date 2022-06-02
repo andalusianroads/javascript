2e236ef28a0be8c3745e6738252842c9d2ff939f
andalusianroads <106731522+andalusianroads@users.noreply.github.com>
2022-06-02 15:33:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ btnCalculate.addEventListener('click', calculate);
 }
 
 const calculate = function () {
-    const priceInt = Number(price.value.replace(',','.').trim());
-    const discountInt = Number(discount.value.replace(',','.').trim());
+    const priceInt = Number(price.value.replace(',','.').replace(/\s/g, ''));
+    const discountInt = Number(discount.value.replace(',','.').replace(/\s/g, ''));
     const discountReduced = priceInt * (discountInt * 0.01);
     console.log(discountReduced);
     const resultNumber = (priceInt - discountReduced);  
